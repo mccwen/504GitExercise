8f96205f896247fcf6020acf50828539be8ac573
mccwen <mccwen2umich.edu>
2022-08-24 10:07:34 -0400

    added the documentation to code.py

--- code.py
+++ code.py
@@ -1,4 +1,13 @@
+''' 
+This code is to calculate the frequency of the sequence.
+First, provide a sequence as the input.
+Second, create an empty dictionary, b.
+Second, check each base in the sequence, set the count as 1 in the first step.
+Third, increment 1 to the count.
+
+'''
 def function1(a):
+# a is the sequence
     b = dict()
     for c in a:
         if c not in b:
@@ -8,7 +17,7 @@ def function1(a):
     return b
 
 def function2(a):
-    print('freqs')
+    print('freqsfind ')
     total = float(sum([a[b] for b in a.keys()]))
     for b in a.keys():
         print(b + ':' + str(a[b]/total))
